50a0a14d79a354bb6098b44e8c065400e37f7722
Youssef S <youssef.sabbagh.dev@gmail.com>
2023-05-18 12:32:34 -0400

    feat: add interface of the login page (Layout)

--- client/src/app/login/LoginImage.tsx
+++ client/src/app/login/LoginImage.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 export const LoginImage = () => {
   return (
     <div className='relative flex h-full w-full items-center justify-center'>
-      {/* <Image src='https://uploads-ssl.webflow.com/64663144fed35c222286c3ae/646631748945f927e2d9a2f8_undraw_online_banking_re_kwqh.svg' alt='login images' fill /> */}
-      <Image src='https://uploads-ssl.webflow.com/64663144fed35c222286c3ae/646631748e7b3894aba6b6d0_undraw_online_payments_re_y8f2.svg' alt='login images' fill />
+      <Image src='https://uploads-ssl.webflow.com/64663144fed35c222286c3ae/646631748945f927e2d9a2f8_undraw_online_banking_re_kwqh.svg' alt='login images' fill /> */}
+      {/* <Image src='https://uploads-ssl.webflow.com/64663144fed35c222286c3ae/646631748e7b3894aba6b6d0_undraw_online_payments_re_y8f2.svg' alt='login images' fill />
     </div>
   )
 }
